86892c2f4af5d687bd33f4a06697156e7f97b482
nandinisangar <nandinisangar6@gmail.com>
2022-04-19 23:59:46 +0530

    final commit

---
+++ app/src/main/java/com/example/quickbikeservices/Select_Bike.java
@@ -0,0 +1,26 @@
+package com.example.quickbikeservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class Select_Bike extends AppCompatActivity {
+    LinearLayout l ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_bike);
+        l = findViewById(R.id.click_bike);
+        l.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Select_Bike.this,Select_brand.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/quickbikeservices/Select_brand.java
@@ -0,0 +1,14 @@
+package com.example.quickbikeservices;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Select_brand extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_brand);
+    }
+}
