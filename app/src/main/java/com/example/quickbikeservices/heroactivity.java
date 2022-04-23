6818b0abf045a0bac2fcc389eb78f04edf5491ad
nandinisangar <nandinisangar6@gmail.com>
2022-04-23 14:53:50 +0530

    final commit

--- app/src/main/java/com/example/quickbikeservices/heroactivity.java
+++ app/src/main/java/com/example/quickbikeservices/heroactivity.java
@@ -3,12 +3,18 @@ package com.example.quickbikeservices;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class heroactivity extends AppCompatActivity {
-
+    private Spinner spinner1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heroactivity);
+        spinner1 = findViewById(R.id.spinner_id1);
+        String [] filters = {"Areas ","Services"};
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item,filters);
+        spinner1.setAdapter(adapter);
     }
 }
